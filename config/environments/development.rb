40dd4b64acb4af1008f087291f60c6563bcdd60c
steven <steven.jun@hotmail.com>
2018-04-07 01:58:36 +0900

    Add mail settings
    please add your env var to /config/local_env.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,17 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['EMAIL'],
+    :password             => ENV['EMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
