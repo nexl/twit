a0f2e000edf65ec29d7f57dc2bfe64ee58b6c53c
steven <steven.jun@hotmail.com>
2018-04-07 00:09:37 +0900

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
