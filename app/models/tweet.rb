8735d2769b1b4bc8a6f66c8f0faba8a49ab3aa9f
steven <steven.jun@hotmail.com>
2018-04-08 01:32:59 +0900

    Add tweet default scope
    order by created_at desc

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,6 @@ class Tweet < ApplicationRecord
   belongs_to :user, :foreign_key => "user_id"
 
   validates :message, :presence => true, :length => {:maximum => 140}
+
+  default_scope{order(created_at: :desc)}
 end
