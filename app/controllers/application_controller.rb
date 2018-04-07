7e15e3da2278594b2ce743138219991ca97d874b
steven <steven.jun@hotmail.com>
2018-04-07 11:32:24 +0900

    Add current_user
    get current logged in user information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 end
