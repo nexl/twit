d084f6d8c8bb22077335376fdb3ed8a699fc2a32
steven <steven.jun@hotmail.com>
2018-04-09 22:53:19 +0900

    Remove require login for edit and update
    don't need login to edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: [:feed, :edit, :update] 
+  before_action :require_login, only: [:feed] 
   before_action :do_not_require_login, only: [:create]
   
   def new
